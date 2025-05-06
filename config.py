49a3f9dd47a6cccf44ebea3472efe32ca1eacfd8
Shinigami-Death-Code <chiragurx@gmail.com>
2025-05-06 13:07:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,4 +18,4 @@ DB_URI = os.environ.get("DB_URI", "mongodb+srv://chiragur:6DTQiEwAwynr6UKp@clust
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', Flase))
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
